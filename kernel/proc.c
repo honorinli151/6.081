785dad3fbc71e68c339f6c59893926e98cd8a4f9
Chenle Li <honorinli151@gmail.com>
2022-07-19 05:30:41 +0000

    Debug lab pgtbl

--- kernel/proc.c
+++ kernel/proc.c
@@ -195,7 +195,8 @@ proc_pagetable(struct proc *p)
   // map the usyscall just below TRAPFRAME
   if (mappages(pagetable, USYSCALL, PGSIZE, 
                (uint64)(p->usyscall), PTE_U | PTE_R) < 0){
-    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
     uvmfree(pagetable, 0);
     return 0;
     }
@@ -210,6 +211,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
